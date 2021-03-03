aa4df71d23bde35d1e92ed0037004997dbf33739
Cosiek <fitek2@o2.pl>
2021-03-03 13:10:33 +0100

    #1: setup provider to use API client

--- nodeping/data_source_contacts.go
+++ nodeping/data_source_contacts.go
@@ -2,55 +2,82 @@ package nodeping
 
 import (
 	"context"
-	//"encoding/json"
-	"fmt"
-	//"net/http"
-	//"strconv"
-	//"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"terraform-nodeping/nodeping_api_client"
 )
 
-func dataSourceContactsRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	fmt.Println(d)
+func dataSourceContactRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*nodeping_api_client.Client)
 
 	var diags diag.Diagnostics
 
+	contact, err := client.GetContact(d.Get("id").(string))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(contact.ID)
+	d.Set("type", contact.Type)
+	d.Set("name", contact.Name)
+	d.Set("customer_id", contact.CustomerId)
+	d.Set("custrole", contact.Custrole)
+
+	addresses := flattenAddresses(&contact.Addresses)
+	if err := d.Set("addresses", &addresses); err != nil {
+		return diag.FromErr(err)
+	}
 	return diags
 }
 
-func dataSourceContacts() *schema.Resource {
+func flattenAddresses(addresses *map[string]nodeping_api_client.Address) []interface{} {
+	if addresses == nil { // return fast if nothing to do
+		return make([]interface{}, 0)
+	}
+
+	flattenedAddresses := make([]interface{}, len(*addresses), len(*addresses))
+	i := 0
+	for addressId, address := range *addresses {
+		println(i, addressId, address.Address)
+		flattenedAddress := make(map[string]interface{})
+		flattenedAddress["id"] = addressId
+		flattenedAddress["address"] = address.Address
+		flattenedAddress["type"] = address.Type
+		flattenedAddress["suppressup"] = address.Suppressup
+		flattenedAddress["suppressdown"] = address.Suppressdown
+		flattenedAddress["suppressfirst"] = address.Suppressfirst
+		flattenedAddress["suppressall"] = address.Suppressall
+
+		flattenedAddresses[i] = flattenedAddress
+		i++
+	}
+
+	return flattenedAddresses
+}
+
+func dataSourceContact() *schema.Resource {
 	return &schema.Resource{
-		ReadContext: dataSourceContactsRead,
+		ReadContext: dataSourceContactRead,
 		Schema: map[string]*schema.Schema{
-			"contacts": &schema.Schema{
-				Type:     schema.TypeMap,
+			"id":          &schema.Schema{Type: schema.TypeString, Required: true},
+			"type":        &schema.Schema{Type: schema.TypeString, Computed: true},
+			"customer_id": &schema.Schema{Type: schema.TypeString, Computed: true},
+			"name":        &schema.Schema{Type: schema.TypeString, Computed: true},
+			"custrole":    &schema.Schema{Type: schema.TypeString, Computed: true},
+			"addresses": &schema.Schema{
+				Type:     schema.TypeList,
 				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"_id": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"type":        &schema.Schema{Type: schema.TypeString, Computed: true},
-						"customer_id": &schema.Schema{Type: schema.TypeString, Computed: true},
-						"name":        &schema.Schema{Type: schema.TypeString, Computed: true},
-						"custrole":    &schema.Schema{Type: schema.TypeString, Computed: true},
-						"addresses": &schema.Schema{
-							Type:     schema.TypeMap,
-							Computed: true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"address":       &schema.Schema{Type: schema.TypeString, Computed: true},
-									"type":          &schema.Schema{Type: schema.TypeString, Computed: true},
-									"suppressup":    &schema.Schema{Type: schema.TypeBool, Computed: true},
-									"suppressdown":  &schema.Schema{Type: schema.TypeBool, Computed: true},
-									"suppressfirst": &schema.Schema{Type: schema.TypeBool, Computed: true},
-									"suppressall":   &schema.Schema{Type: schema.TypeBool, Computed: true},
-								},
-							},
-						},
+						"id":            &schema.Schema{Type: schema.TypeString, Computed: true},
+						"address":       &schema.Schema{Type: schema.TypeString, Computed: true},
+						"type":          &schema.Schema{Type: schema.TypeString, Computed: true},
+						"suppressup":    &schema.Schema{Type: schema.TypeBool, Computed: true},
+						"suppressdown":  &schema.Schema{Type: schema.TypeBool, Computed: true},
+						"suppressfirst": &schema.Schema{Type: schema.TypeBool, Computed: true},
+						"suppressall":   &schema.Schema{Type: schema.TypeBool, Computed: true},
 					},
 				},
 			},
--- nodeping/provider.go
+++ nodeping/provider.go
@@ -1,14 +1,34 @@
 package nodeping
 
 import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"terraform-nodeping/nodeping_api_client"
 )
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"token": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Sensitive:   true,
+				DefaultFunc: schema.EnvDefaultFunc("NODEPING_API_TOKEN", nil),
+			},
+		},
 		ResourcesMap: map[string]*schema.Resource{},
 		DataSourcesMap: map[string]*schema.Resource{
-			"nodeping_contacts": dataSourceContacts(),
+			"nodeping_contact": dataSourceContact(),
 		},
+		ConfigureContextFunc: providerConfigure,
 	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	client := nodeping_api_client.NewClient(d.Get("token").(string))
+	var diags diag.Diagnostics // TODO: is diags really needed?
+	return client, diags
 }
