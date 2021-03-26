39e2053f9112f5de0b946c52067beb85cb4d5c93
Witold Majchrzak <w.majchrzak@softkraft.co>
2021-03-26 09:55:54 +0100

    #3: refactor API client

--- nodeping_api_client/api_client.go
+++ nodeping_api_client/api_client.go
@@ -1,10 +1,12 @@
 package nodeping_api_client
 
 import (
+	"context"
 	b64 "encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"time"
 )
@@ -30,13 +32,19 @@ func NewClient(token string) *Client {
 }
 
 func (client *Client) doRequest(req *http.Request) ([]byte, error) {
+	// set request authentication.
+	// from API docs: The password will be ignored so you can either leave it
+	// blank or pass a random string
 	authStr := b64.StdEncoding.EncodeToString([]byte(client.Token + ":whatever"))
 	req.Header.Set("Authorization", "Basic "+authStr)
 
+	// send
 	res, err := client.HTTPClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
+
+	// cleanup
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
@@ -46,6 +54,98 @@ func (client *Client) doRequest(req *http.Request) ([]byte, error) {
 
 	if res.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("status: %d, body: %s", res.StatusCode, body)
+	}
+
+	return body, err
+}
+
+func (client *Client) doRequest2(ctx context.Context, method string, url string, data []byte) ([]byte, error) {
+	request, err := client.prepareRequest2(ctx, method, url, data)
+	if err != nil {
+		return nil, err
+	}
+	body, err := client.sendRequest(request)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func (client *Client) prepareRequest(method string, url string, data []byte) (*http.Request, error) {
+	// prepare data to be sent
+	var dataReader *strings.Reader
+	if data != nil {
+		dataReader = strings.NewReader(string(data))
+	}
+
+	// initialize request
+	reqest, err := http.NewRequest(method, url, dataReader)
+	if err != nil {
+		return nil, err
+	}
+
+	// set request authentication.
+	// from API docs: The password will be ignored so you can either leave it
+	// blank or pass a random string
+	authStr := b64.StdEncoding.EncodeToString([]byte(client.Token + ":whatever"))
+	reqest.Header.Set("Authorization", "Basic "+authStr)
+
+	// set json content type for request that have data to be sent
+	if data != nil {
+		reqest.Header.Set("Content-Type", "application/json")
+	}
+
+	return reqest, nil
+}
+
+func (client *Client) prepareRequest2(ctx context.Context, method string, url string, data []byte) (*http.Request, error) {
+	// prepare data to be sent
+	var body *strings.Reader
+	if data != nil {
+		body = strings.NewReader(string(data))
+	} else {
+		body = strings.NewReader("")
+	}
+
+	// initialize request
+	reqest, err := http.NewRequestWithContext(ctx, method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	// set request authentication.
+	// from API docs: The password will be ignored so you can either leave it
+	// blank or pass a random string
+	authStr := b64.StdEncoding.EncodeToString([]byte(client.Token + ":whatever"))
+	reqest.Header.Set("Authorization", "Basic "+authStr)
+
+	// set json content type for request that have data to be sent
+	if data != nil {
+		reqest.Header.Set("Content-Type", "application/json")
+	}
+
+	return reqest, nil
+}
+
+func (client *Client) sendRequest(request *http.Request) ([]byte, error) {
+	// send
+	response, err := client.HTTPClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// cleanup
+	defer response.Body.Close()
+
+	// read response
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// error handling
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status: %d, body: %s", response.StatusCode, body)
 	}
 
 	return body, err
