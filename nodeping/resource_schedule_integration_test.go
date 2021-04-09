9fc90a52b2a6f2d3ba29b54b534869687eb7e73c
Witold Majchrzak <w.majchrzak@softkraft.co>
2021-04-09 17:03:08 +0200

    #3: add integration tests for schedules

---
+++ nodeping/resource_schedule_integration_test.go
@@ -0,0 +1,160 @@
+package nodeping
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+
+	apiClient "terraform-nodeping/nodeping_api_client"
+)
+
+//func TestTerraformScheduleLifeCycle(t *testing.T) {
+func TestTsSLC(t *testing.T) {
+	const terraformDir = "testdata/schedules_integration"
+	const terraformMainFile = terraformDir + "/main.tf"
+
+	// create main.tf
+	copyFile(terraformDir+"/step_1", terraformMainFile)
+
+	// initialize terraform
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: terraformDir,
+		MaxRetries:   0,
+		Upgrade:      true,
+	})
+	terraform.Init(t, terraformOptions)
+
+	// prepare cleanup
+	defer cleanupTerraformDir(terraformDir)
+
+	// prepare API client
+	token := os.Getenv("NODEPING_API_TOKEN")
+	client := apiClient.NewClient(token)
+
+	// prepare context for client
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// -----------------------------------
+	// create a single schedule
+	terraform.Apply(t, terraformOptions)
+
+	scheduleName := terraform.Output(t, terraformOptions, "first_schedule_name")
+
+	schedule, err := client.GetSchedule(ctx, scheduleName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, scheduleName, schedule.Name)
+	assert.Equal(t, "FirstSchedule", schedule.Name)
+	assert.Equal(t, 7, len(schedule.Data))
+	// check days
+	assert.Equal(t, schedule.Data["monday"]["time1"], "6:00")
+	assert.Equal(t, schedule.Data["monday"]["time2"], "7:00")
+	assert.Equal(t, schedule.Data["monday"]["exclude"], false)
+	assert.Equal(t, schedule.Data["monday"]["disabled"], false)
+	assert.Equal(t, schedule.Data["monday"]["allday"], false)
+
+	assert.Equal(t, schedule.Data["saturday"]["time1"], "6:00")
+	assert.Equal(t, schedule.Data["saturday"]["time2"], "7:00")
+	assert.Equal(t, schedule.Data["saturday"]["exclude"], true)
+	assert.Equal(t, schedule.Data["saturday"]["disabled"], false)
+	assert.Equal(t, schedule.Data["saturday"]["allday"], false)
+
+	assert.Equal(t, schedule.Data["sunday"]["time1"], "")
+	assert.Equal(t, schedule.Data["sunday"]["time2"], "")
+	assert.Equal(t, schedule.Data["sunday"]["exclude"], false)
+	assert.Equal(t, schedule.Data["sunday"]["disabled"], false)
+	assert.Equal(t, schedule.Data["sunday"]["allday"], true)
+
+	// -----------------------------------
+	// change schedule data
+	copyFile(terraformDir+"/step_2", terraformMainFile)
+	terraform.Apply(t, terraformOptions)
+
+	assert.Equal(t, scheduleName, terraform.Output(t, terraformOptions, "first_schedule_name"))
+
+	schedule, err = client.GetSchedule(ctx, scheduleName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, scheduleName, schedule.Name)
+	assert.Equal(t, 7, len(schedule.Data))
+	// check days
+	assert.Equal(t, schedule.Data["monday"]["time1"], "16:00")
+	assert.Equal(t, schedule.Data["monday"]["time2"], "17:00")
+	assert.Equal(t, schedule.Data["monday"]["exclude"], false)
+	assert.Equal(t, schedule.Data["monday"]["disabled"], false)
+	assert.Equal(t, schedule.Data["monday"]["allday"], false)
+
+	assert.Equal(t, schedule.Data["saturday"]["time1"], "")
+	assert.Equal(t, schedule.Data["saturday"]["time2"], "")
+	assert.Equal(t, schedule.Data["saturday"]["exclude"], false)
+	assert.Equal(t, schedule.Data["saturday"]["disabled"], false)
+	assert.Equal(t, schedule.Data["saturday"]["allday"], true)
+
+	assert.Equal(t, schedule.Data["sunday"]["time1"], "")
+	assert.Equal(t, schedule.Data["sunday"]["time2"], "")
+	assert.Equal(t, schedule.Data["sunday"]["exclude"], false)
+	assert.Equal(t, schedule.Data["sunday"]["disabled"], false)
+	assert.Equal(t, schedule.Data["sunday"]["allday"], true)
+	// -----------------------------------
+	// change schedule data once more
+	copyFile(terraformDir+"/step_3", terraformMainFile)
+	terraform.Apply(t, terraformOptions)
+
+	assert.Equal(t, scheduleName, terraform.Output(t, terraformOptions, "first_schedule_name"))
+
+	schedule, err = client.GetSchedule(ctx, scheduleName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, scheduleName, schedule.Name)
+	assert.Equal(t, 1, len(schedule.Data))
+	// check days
+	assert.Equal(t, nil, schedule.Data["monday"]["time1"])
+	assert.Equal(t, nil, schedule.Data["monday"]["time2"])
+	assert.Equal(t, nil, schedule.Data["monday"]["exclude"])
+	assert.Equal(t, nil, schedule.Data["monday"]["disabled"])
+	assert.Equal(t, nil, schedule.Data["monday"]["allday"])
+
+	assert.Equal(t, "6:00", schedule.Data["sunday"]["time1"])
+	assert.Equal(t, "7:00", schedule.Data["sunday"]["time2"])
+	assert.Equal(t, false, schedule.Data["sunday"]["exclude"])
+	assert.Equal(t, false, schedule.Data["sunday"]["disabled"])
+	assert.Equal(t, false, schedule.Data["sunday"]["allday"])
+	// -----------------------------------
+	// destroy
+	terraform.Destroy(t, terraformOptions)
+}
+
+func copyFile(src, dst string) {
+	data, err := ioutil.ReadFile(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile(dst, data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func cleanupTerraformDir(terraformDir string) {
+	err := os.RemoveAll(terraformDir + "/.terraform")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fileName := range []string{".terraform.lock.hcl", "main.tf",
+		"terraform.tfstate", "terraform.tfstate.backup"} {
+		err = os.Remove(terraformDir + "/" + fileName)
+		if err != nil {
+			//log.Fatal(err)
+		}
+	}
+}
