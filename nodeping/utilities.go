e01fb4aee5fa747a27241ef8910ae6242eeb005d
Witold Majchrzak <w.majchrzak@softkraft.co>
2021-04-09 17:51:00 +0200

    #3: move some test utilities to a separate go file

---
+++ nodeping/utilities.go
@@ -0,0 +1,32 @@
+package nodeping
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func copyFile(src, dst string) {
+	data, err := ioutil.ReadFile(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile(dst, data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func cleanupTerraformDir(terraformDir string) {
+	err := os.RemoveAll(terraformDir + "/.terraform")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fileName := range []string{".terraform.lock.hcl", "main.tf",
+		"terraform.tfstate", "terraform.tfstate.backup"} {
+		err = os.Remove(terraformDir + "/" + fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
