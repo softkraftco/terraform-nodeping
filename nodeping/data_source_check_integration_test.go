1798e23992b781c14583a8d40013ca292bb3721c
Witold Majchrzak <w.majchrzak@softkraft.co>
2021-04-29 12:00:39 +0200

    #3: add tests for data sources

---
+++ nodeping/data_source_check_integration_test.go
@@ -0,0 +1,79 @@
+package nodeping
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+
+	apiClient "terraform-nodeping/nodeping_api_client"
+)
+
+func TestCheckDataSource(t *testing.T) {
+	// prepare API client
+	token := os.Getenv("NODEPING_API_TOKEN")
+	client := apiClient.NewClient(token)
+
+	// prepare a single HTTP check
+	notifications := make([]map[string]apiClient.Notification, 1)
+	nmap := make(map[string]apiClient.Notification, 1)
+	nmap["exmp"] = apiClient.Notification{
+		Delay:    5,
+		Schedule: "Days",
+	}
+	notifications[0] = nmap
+
+	checkUpdate := apiClient.CheckUpdate{
+		Label:         "TheCheck",
+		Type:          "HTTP",
+		Target:        "http://example.com",
+		Enable:        "inactive",
+		Public:        "false",
+		RunLocations:  []string{"nam"},
+		Notifications: notifications,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	check, err := client.CreateCheck(ctx, &checkUpdate)
+	if err != nil {
+		log.Fatal(err)
+	}
+	println("******")
+	println(check.Notifications)
+	println("******")
+	// prepare cleanup
+	defer client.DeleteCheck(ctx, check.ID)
+
+	const terraformDir = "testdata/checks_integration"
+	const terraformMainFile = terraformDir + "/main.tf"
+
+	// create main.tf
+	copyFile(terraformDir+"/data_source", terraformMainFile)
+
+	// initialize terraform
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: terraformDir,
+		MaxRetries:   1,
+		Vars:         map[string]interface{}{"check_id": check.ID},
+	})
+	terraform.Init(t, terraformOptions)
+
+	// prepare cleanup
+	defer cleanupTerraformDir(terraformDir)
+
+	// -----------------------------------
+	// use data source
+	terraform.Apply(t, terraformOptions)
+
+	assert.Equal(t, check.ID, terraform.Output(t, terraformOptions, "check_id"))
+	assert.Equal(t, check.Type, terraform.Output(t, terraformOptions, "check_type"))
+	assert.Equal(t, check.Parameters["target"].(string), terraform.Output(t, terraformOptions, "check_target"))
+	assert.Equal(t, check.Enable, terraform.Output(t, terraformOptions, "check_enable"))
+	assert.Equal(t, "false", terraform.Output(t, terraformOptions, "check_public"))
+	assert.Equal(t, "nam", terraform.Output(t, terraformOptions, "check_runlocations"))
+}
---
+++ nodeping/data_source_schedule_integration_test.go
@@ -0,0 +1,66 @@
+package nodeping
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestScheduleDataSource(t *testing.T) {
+	const terraformDir = "testdata/schedules_integration"
+	const terraformMainFile = terraformDir + "/main.tf"
+
+	// create main.tf
+	copyFile(terraformDir+"/data_source", terraformMainFile)
+
+	// initialize terraform
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: terraformDir,
+		MaxRetries:   0,
+		Upgrade:      true,
+	})
+	terraform.Init(t, terraformOptions)
+
+	// prepare cleanup
+	defer cleanupTerraformDir(terraformDir)
+
+	// -----------------------------------
+	// read a schedule - "Weekdays" is available by default
+	terraform.Apply(t, terraformOptions)
+
+	assert.Equal(t, "Weekdays", terraform.Output(t, terraformOptions, "schedule_name"))
+
+	// get schedule from output
+	output := terraform.OutputJson(t, terraformOptions, "schedule")
+
+	var outputObj map[string]interface{}
+	json.Unmarshal([]byte(output), &outputObj)
+
+	// check content
+	assert.Equal(t, "Weekdays", outputObj["name"].(string))
+	assert.Equal(t, "Weekdays", outputObj["id"].(string))
+	assert.Equal(t, "", outputObj["customer_id"].(string))
+
+	// unwrap "data"
+	weekdays := []string{"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"}
+	for _, dayMapInterface := range outputObj["data"].([]interface{}) {
+		dayMap := dayMapInterface.(map[string]interface{})
+		day := dayMap["day"].(string)
+		assert.Contains(t, weekdays, day)
+		if day == "saturday" || day == "sunday" {
+			assert.False(t, dayMap["allday"].(bool))
+			assert.True(t, dayMap["disabled"].(bool))
+			assert.False(t, dayMap["exclude"].(bool))
+			assert.Equal(t, "12:00", dayMap["time1"].(string))
+			assert.Equal(t, "12:00", dayMap["time2"].(string))
+		} else {
+			assert.True(t, dayMap["allday"].(bool))
+			assert.False(t, dayMap["disabled"].(bool))
+			assert.False(t, dayMap["exclude"].(bool))
+			assert.Equal(t, "0:00", dayMap["time1"].(string))
+			assert.Equal(t, "23:59", dayMap["time2"].(string))
+		}
+	}
+}
